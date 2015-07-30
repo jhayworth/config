c17a7b2b7cd987cf8236b03e3e739119f9f8e3c9
Joshua Hayworth <joshua.hayworth@kratosnetworks.com>
2015-07-29 18:32:30 -0700

    Add which-mode-func minor mode to c mode hook

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,6 +17,10 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/color-themes")
 (load-theme 'noctilux t)
 
+;; Fun startup message
+(defun display-startup-echo-area-message ()
+  (message "Let the hacking begin!"))
+
 ;; Marmalade package management
 ;;(require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
@@ -28,9 +32,9 @@
 (require 'ido)
 (ido-mode t)
 
-(require 'whitespace)
+;;(require 'whitespace)
 ;;(require 'highlight-symbol)
-(require 'minimap)
+;;(require 'minimap)
 
 ;; magit mode
 ;;(require 'magit)
@@ -74,7 +78,10 @@
 	    (show-paren-mode 1)
 
 	    ;; turn on line numbers
-	    (linum-mode t)))
+	    (linum-mode t)
+
+            ;; show the current function
+            (which-function-mode t)))
 
 ;; JavaScript specific settings
 (add-hook 'js-mode-hook
@@ -92,30 +99,67 @@
 	    (hs-minor-mode t)))
 
 ;; General cleanup
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+;;(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;; Set smooth scrolling
 (setq scroll-step 1
       scroll-conservatively 10000)
 
 ;; Autocreation of new lines at the end of a buffer
-(setq next-line-add-newlines t)
+;;(setq next-line-add-newlines t)
+
+;; ***************** EDITING FUNCTIONS *****************
+
+;; http://www.emacswiki.org/emacs/DeletingWhitespace
+(defun joshua-delete-leading-whitespace (start end)
+  "Delete whitespace at the beginning of each line in region."
+  (interactive "*r")
+  (save-excursion
+    (if (not (bolp)) (forward-line 1))
+    (delete-whitespace-rectangle (point) end nil)))
+
+;; http://www.emacswiki.org/BackwardKillLine
+(defun joshua-backward-kill-line (arg)
+  "Kill ARG lines backward."
+  (interactive "p")
+  (kill-line (- 1 arg)))
 
 ;; Custom Keybindings
 (global-set-key "\C-c\C-e" 'eval-expression)
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-d\C-l" 'joshua-delete-leading-whitespace)
+(global-set-key "\C-c\C-d\C-b" 'joshua-backward-kill-line)
 (global-set-key (kbd "S-C-<right>") 'shrink-window-horizontally)
 (global-set-key (kbd "S-C-<left>") 'enlarge-window-horizontally)
 (global-set-key (kbd "S-C-<down>") 'shrink-window)
 (global-set-key (kbd "S-C-<up>") 'enlarge-window)
 
+;; Org Mode keybindings
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
 (global-set-key (kbd "<escape>") 'save-buffers-kill-terminal)
 (global-set-key [f5] 'compile)
-;;(global-set-key [f3] 'highlight-symbol-at-point)
-;;(global-set-key [(control f3)] 'highlight-symbol-next)
-;;(global-set-key [(shift f3)] 'highlight-symbol-prev)
+(global-set-key [f3] 'highlight-symbol-at-point)
+(global-set-key [(control f3)] 'highlight-symbol-next)
+(global-set-key [(shift f3)] 'highlight-symbol-prev)
 ;;(global-set-key [(meta f3)] 'highlight-symbol-query-replace)
 (global-set-key [f12] 'call-last-kbd-macro)
 
 (delete-selection-mode t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(which-func ((t (:foreground "light gray")))))
