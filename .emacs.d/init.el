81bb0d1f5d6c28888ccdae964048c14100d48b80
Joshua Hayworth <joshua@hayworthfamily.com>
2015-08-13 21:53:32 -0700

    Added highlight-symbol, abstracted out c-mode customizations

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -13,6 +13,9 @@
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 
+;; Source load path
+(add-to-list 'load-path (expand-file-name "~/dotfiles/emacs"))
+
 ;; Theme load path
 (add-to-list 'custom-theme-load-path "~/.emacs.d/color-themes")
 (load-theme 'noctilux t)
@@ -29,8 +32,10 @@
 (ido-mode t)
 
 (require 'whitespace)
-;;(require 'highlight-symbol)
+(require 'highlight-symbol)
 (require 'minimap)
+(require 'joshua-c)
+
 
 ;; magit mode
 ;;(require 'magit)
@@ -62,20 +67,6 @@
 (set-default 'truncate-partial-width-windows nil)
 
 ;; ***************** HOOKS *****************
-;; Common language hook
-(add-hook 'c-mode-common-hook
-	  (lambda ()
-	    ;; 4 space tabs, insert spaces
-	    (setq-default c-default-style "linux"
-			  indent-tabs-mode nil
-			  c-basic-offset 4)
-
-	    ;; turn on 'show matching parens'
-	    (show-paren-mode 1)
-
-	    ;; turn on line numbers
-	    (linum-mode t)))
-
 ;; JavaScript specific settings
 (add-hook 'js-mode-hook
 	  (lambda ()
@@ -112,10 +103,13 @@
 
 (global-set-key (kbd "<escape>") 'save-buffers-kill-terminal)
 (global-set-key [f5] 'compile)
-;;(global-set-key [f3] 'highlight-symbol-at-point)
-;;(global-set-key [(control f3)] 'highlight-symbol-next)
-;;(global-set-key [(shift f3)] 'highlight-symbol-prev)
+(global-set-key [f3] 'highlight-symbol-at-point)
+(global-set-key [(control f3)] 'highlight-symbol-next)
+(global-set-key [(shift f3)] 'highlight-symbol-prev)
 ;;(global-set-key [(meta f3)] 'highlight-symbol-query-replace)
 (global-set-key [f12] 'call-last-kbd-macro)
 
 (delete-selection-mode t)
+
+
+(shell)
---
+++ emacs/joshua-c.el
@@ -0,0 +1,16 @@
+(provide 'joshua-c)
+
+(add-hook 'c-mode-common-hook
+    (lambda ()
+
+      ;; 4 space tabs, insert spaces
+      (setq-default c-default-style "linux"
+                    indent-tabs-mode nil
+                    c-basic-offset 4)
+
+      ;; turn on 'show matching parens'
+      (show-paren-mode 1)
+
+      ;; turn on line numbers
+      (linum-mode t)
+))
