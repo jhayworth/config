fbc5c9db9b9e28f1f08aab003ae511b712c72de0
Joshua Hayworth <joshua.hayworth@kratosnetworks.com>
2015-08-14 18:06:34 -0700

    Adding grey font color to which-func c minor mode

--- emacs/joshua-c.el
+++ emacs/joshua-c.el
@@ -16,4 +16,17 @@
 
       ;; Show the current function
       (which-function-mode t)
+
+      (custom-set-variables
+       ;; custom-set-variables was added by Custom.
+       ;; If you edit it by hand, you could mess it up, so be careful.
+       ;; Your init file should contain only one such instance.
+       ;; If there is more than one, they won't work right.
+       )
+      (custom-set-faces
+       ;; custom-set-faces was added by Custom.
+       ;; If you edit it by hand, you could mess it up, so be careful.
+       ;; Your init file should contain only one such instance.
+       ;; If there is more than one, they won't work right.
+       '(which-func ((t (:foreground "light gray")))))
 ))
