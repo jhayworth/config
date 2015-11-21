5d3b88940f8aff4e59e3d4decb77f1ca91ce8a90
Joshua Hayworth <joshua.hayworth@kratosnetworks.com>
2015-11-20 19:14:55 -0800

    Tweaks to ediff settings

--- emacs/joshua-diff.el
+++ emacs/joshua-diff.el
@@ -1,6 +1,17 @@
 (provide 'joshua-diff)
 
 (defun joshua-command-line-diff (switch)
-      (let ((file1 (pop command-line-args-left))
-            (file2 (pop command-line-args-left)))
-        (ediff file1 file2)))
+  (let ((file1 (pop command-line-args-left))
+        (file2 (pop command-line-args-left)))
+
+  (ediff file1 file2)))
+
+(setq ediff-split-window-function (quote split-window-horizontally))
+
+(add-hook 'ediff-prepare-buffer-hook
+	  (lambda()
+
+	    ;; turn on line numbers
+	    (setq linum-format "%d ") (quote linum-mode)
+	  )
+)
