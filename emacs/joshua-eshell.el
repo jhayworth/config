f39bdfc2d5d4e1a989ebf55290e507cca4d0f95f
Joshua Hayworth <jhayworth@btisinc.com>
2019-03-18 01:19:37 -0700

    eshell config

---
+++ emacs/joshua-eshell.el
@@ -0,0 +1,31 @@
+;; http://www.howardism.org/Technical/Emacs/eshell-fun.html
+
+(provide 'joshua-eshell)
+
+(defun eshell-here ()
+  "Opens up a new shell in the directory associated with the
+current buffer's file. The eshell is renamed to match that
+directory to make multiple eshell windows easier."
+  (interactive)
+  (let* ((parent (if (buffer-file-name)
+                     (file-name-directory (buffer-file-name))
+                   default-directory))
+         (height (/ (window-total-height) 3))
+         (name   (car (last (split-string parent "/" t)))))
+    (split-window-vertically (- height))
+    (other-window 1)
+    (eshell "new")
+    (rename-buffer (concat "*eshell: " name "*"))
+
+    (insert (concat "ls"))
+    (eshell-send-input)))
+
+(defun eshell/x ()
+  (insert "exit")
+  (eshell-send-input)
+  (delete-window))
+
+(defalias 'ff 'find-file)
+(defalias 'ffo 'find-file-other-window)
+
+(global-set-key (kbd "C-!") 'eshell-here)
