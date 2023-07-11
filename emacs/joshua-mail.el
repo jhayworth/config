96601556c8bc7f3544870a4015dd07e3d6648de4
Joshua Hayworth <joshua@hayworthfamily.com>
2023-07-10 21:49:02 -0700

    [mu4e] Turning address autocompletion off

--- emacs/joshua-mail.el
+++ emacs/joshua-mail.el
@@ -13,9 +13,6 @@
 ;; you use e.g. 'mu mkdir' to make the Maildirs if they don't
 ;; already exist
 
-;; below are the defaults; if they do not exist yet, mu4e offers to
-;; create them. they can also functions; see their docstrings.
-(setq user-mail-address "joshua@scriptablecamera.com")
 (setq mu4e-sent-folder   "/sent")
 (setq mu4e-drafts-folder "/drafts")
 (setq mu4e-trash-folder  "/trash")
@@ -24,12 +21,17 @@
 (setq send-mail-function 'sendmail-send-it)
 (setq message-send-mail-function 'message-send-mail-with-sendmail)
 
+;; Turn off address autocompletion feature
+(setq mu4e-compose-complete-addresses nil)
+
 ;; the maildirs you use frequently; access them with 'j' ('jump')
 (setq   mu4e-maildir-shortcuts
-    '((:maildir "/archive" :key ?a)
-      (:maildir "/inbox"   :key ?i)
-      (:maildir "/work"    :key ?w)
-      (:maildir "/sent"    :key ?s)))
+	'(
+	  (:maildir "/" :key ?r)
+	  (:maildir "/archive" :key ?a)
+          (:maildir "/inbox"   :key ?i)
+          (:maildir "/work"    :key ?w)
+          (:maildir "/sent"    :key ?s)))
 
 ;; don't keep message buffers around
 (setq message-kill-buffer-on-exit t)
@@ -38,5 +40,36 @@
 (with-eval-after-load "mm-decode"
   (add-to-list 'mm-discouraged-alternatives "text/html")
   (add-to-list 'mm-discouraged-alternatives "text/richtext"))
+
+;; View email in Firefox
+(setq browse-url-browser-function 'browse-url-generic)
+(setq browse-url-generic-program "firefox")
+
+(setq mu4e-contexts
+    `( ,(make-mu4e-context
+          :name "Private"
+          :enter-func (lambda () (mu4e-message "Entering Private context"))
+          :leave-func (lambda () (mu4e-message "Leaving Private context"))
+          :vars '( ( user-mail-address	    . "joshua@hayworthfamily.com"  )
+                   ( user-full-name	    . "Joshua Hayworth" )
+                   )
+	  )
+       
+       ,(make-mu4e-context
+          :name "Work"
+          :enter-func (lambda () (mu4e-message "Entering Work context"))
+	  :leave-func (lambda () (mu4e-message "Leaving Work context"))
+          :vars '( ( user-mail-address	     . "joshua@scriptablecamera.com" )
+                   ( user-full-name	     . "Joshua Hayworth" )
+                   ( mu4e-compose-signature  .
+                     (concat
+                       "Joshua Hayworth\n"
+                       "Owner, Software Developer\n"))))
+      )
+ )
+
+;; compose with the current context is no context matches;
+;; default is to ask
+(setq mu4e-compose-context-policy nil)
 
 (provide 'joshua-mail)
